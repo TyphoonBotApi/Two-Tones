fd1672c78214a6759e52260e1c82867029e9b381
TyphoonBotApi <45461862+TyphoonBotApi@users.noreply.github.com>
2018-12-03 16:27:42 +0000

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,51 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var badWords = [
+  'fuck',
+  'shit',
+  'dick',
+  'bitch'
+];
+
+bot.on('message', message => {
+  var words = message.content.toLowerCase().trim().match(/\w+|\s+|[^\s\w]+/g);
+  var containsBadWord = words.some(word => {
+    return badWords.includes(word);
+  });
+  if (containsBadWord) {
+    message.delete(1);
+    message.channel.send("That word's not appropriate!");
+  }
+});
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+ client.on('message', message => {
+    if (message.content === '!staff') {
+    	message.reply('https://bit.ly/DinoStaff');
+  	}
+}); 
+
+ client.on('message', message => {
+    if (message.content === '!channel') {
+    	message.reply('https://www.youtube.com/channel/UCXXwv_TOATDZdHNuq-lpLgw');
+  	}
+}); 
+
+ client.on('message', message => {
+    if (message.content === '!yoshi') {
+    	message.reply('Yoshirocks the best dinosaur Minecraft  YouTuber you will ever,A jam packed channel with new and awesome content,this wholesome youtuber will have you hooked for hours,Drop a like on his videos for a cookie,he works damn hard  on his videos so you better subscribe!');
+  	}
+}); 
+
+
+client.login(process.env.BOT_TOKEN);
