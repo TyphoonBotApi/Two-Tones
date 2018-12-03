0ee4ada447f81d187519bc93bf77cdba3d070690
TyphoonBotApi <45461862+TyphoonBotApi@users.noreply.github.com>
2018-12-03 16:35:10 +0000

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+ client.on('message', message => {
+    if (message.content === '!staff') {
+    	message.reply('https://bit.ly/DinoStaff');
+  	}
+}); 
+
+ client.on('message', message => {
+    if (message.content === '!channel') {
+    	message.reply('https://www.youtube.com/channel/UCXXwv_TOATDZdHNuq-lpLgw');
+  	}
+}); 
+
+ client.on('message', message => {
+    if (message.content === '!yoshi') {
+    	message.reply('Yoshirocks the best dinosaur Minecraft  YouTuber you will ever,A jam packed channel with new and awesome content,this wholesome youtuber will have you hooked for hours,Drop a like on his videos for a cookie,he works damn hard  on his videos so you better subscribe!');
+  	}
+}); 
+
+client.on('message', message => { if (message.content.startsWith("!say")) { message.delete(1000);  message.channel.send(message.content.slice(4, message.content.length)); } 
+});
+
+client.on('message', message => { if (message.content.startsWith("fuck")) { message.delete(1000); 
+
+client.login(process.env.BOT_TOKEN);
