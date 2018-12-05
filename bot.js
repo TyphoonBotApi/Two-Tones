aada544d4ea17855e7f515971726feb728a25c2f
TyphoonBotApi <45461862+TyphoonBotApi@users.noreply.github.com>
2018-12-05 08:58:03 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-bot.on('ready', () => { bot.user.setGame('GAME HERE') })
-
-  
-  
-
-
  
-
 client.on('ready', () => {
     console.log('I am ready!');
 });
