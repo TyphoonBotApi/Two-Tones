0f7529d5a688b70d407e9461aa69f10668e4da8c
TyphoonBotApi <45461862+TyphoonBotApi@users.noreply.github.com>
2018-12-05 13:53:50 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,17 +9,11 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '!info') {
-    	message.reply('Two Tone Rebel V1.1');
+    	message.reply('Two Tone Rebel V1.3');
   	}
 });
     
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	get ping() {
-    return this.pings.reduce((prev, p) => prev + p, 0) / this.pings.length;
-  }
-  	
-});
+
 
  client.on('message', message => {
     if (message.content === '!staff') {
