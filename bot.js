f02810ee4e43640c61cd983e9b522bbf077917fc
TyphoonBotApi <45461862+TyphoonBotApi@users.noreply.github.com>
2018-12-12 17:28:18 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
-
-client.user.setActivity("Game");
+client.user.setActivity("Game"
 
 client.on('message', message => {
     if (message.content === '!info') {
