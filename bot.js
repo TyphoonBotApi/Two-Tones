99c5c7e115577207c361495bb1b3939c61c7522e
TyphoonBotApi <45461862+TyphoonBotApi@users.noreply.github.com>
2018-12-14 08:43:48 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,11 +8,6 @@ client.on('ready', () => {
 
 client.on('message', message => {
     
-    if (message.content === '!embed') {
-    message.channel.send({embed: {
-  color: 3447003,
-  description: "A very simple Embed!"
-}
     
     if (message.content === '!info') {
     	message.reply('Two Tone Rebel V1.3');
